9a833ecbec452a0f4b20c60946e732d0b67f4008
Ryan Bender <152329584+bendery13@users.noreply.github.com>
2026-02-22 13:40:34 -0600

    Added slingshot, projectile, and Slinghsot.cs - through pg 693

---
+++ Assets/Scenes/Slingshot.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    public GameObject launchPoint;
+
+    void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+    }
+    void OnMouseEnter()
+    {
+        // print("Slingshot: OneMouseEnter()");
+        launchPoint.SetActive(true);
+    }
+    void OnMouseExit()
+    {
+        // print("Slingshot: OnMouseExit()");
+        launchPoint.SetActive(false);
+    }
+}
