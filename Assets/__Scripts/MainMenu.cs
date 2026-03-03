5257f3d5f04111f44babcef2d3e35d1e7b64e830
Ryan Bender <152329584+bendery13@users.noreply.github.com>
2026-03-03 15:33:50 -0600

    Added Main Menu

---
+++ Assets/__Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void ExitButton()
+    {
+        Application.Quit();
+        Debug.Log("Exit Game");
+    }
+    public void StartButton()
+    {
+        SceneManager.LoadScene("_Scene_0");
+    }
+}
